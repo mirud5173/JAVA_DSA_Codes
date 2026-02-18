97caf8bcb3b07d515c6a6b70ad8677cd3e5fe412
Mirudhula <141794395+mirud5173@users.noreply.github.com>
2026-02-18 20:53:44 +0530

    Corrected code

--- Star_Patterns/Hollow_Diamond.java
+++ Star_Patterns/Hollow_Diamond.java
@@ -12,18 +12,24 @@ public class Hollow_Diamond {
             for(int k=1;k<=n-i;k++)
                 System.out.print(" ");
             System.out.print("*");
-            for(int j=0;j<i-1;j++)
-                System.out.print("  ");
+            for(int j=1;j<=1+(i-2)*2;j++)
+                System.out.print(" ");
             System.out.print("*");
             System.out.println();
         }
-        for(int i=1;i<n;i++){
-            for(int k=1;k<=i;k++)
+        for(int i=n-1;i>1;i--){
+            for(int k=1;k<=n-i;k++)
                 System.out.print(" ");
-            for(int j=n-1;j>=i;j--)
-                System.out.print("* ");
+            System.out.print("*");
+            for(int j=1;j<=1+(i-2)*2;j++)
+                System.out.print(" ");
+            System.out.print("*");
             System.out.println();
         }
+        for(int i=1;i<n;i++)
+            System.out.print(" ");
+        System.out.print("*");
+        System.out.println();
         sc.close();
     }
 }
